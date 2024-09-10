c539cf00ad515c1a33a71997c621df2d680daef7
Yun-Da Chen <chenyunda218@gmail.com>
2024-09-10 19:58:09 +0800

    feat

--- lib/authentik_client.ex
+++ lib/authentik_client.ex
@@ -12,8 +12,8 @@ defmodule AuthentikClient do
       :world
 
   """
-  @spec new(Keyword.t()) :: map()
-  def new(configs) do
+  @spec new_client(Keyword.t()) :: map()
+  def new_client(configs) do
     %{
       client_id: Application.get_env(Authentik, :client_id, Keyword.get(configs, :client_id, "")),
       client_secret:
@@ -21,4 +21,48 @@ defmodule AuthentikClient do
       base_url: Application.get_env(Authentik, :base_url, Keyword.get(configs, :base_url, ""))
     }
   end
+
+  def get_user_info(%{base_url: base_url}, access_token) do
+    Req.get("#{base_url}/application/o/userinfo/",
+      headers: %{authorization: "Bearer #{access_token}"}
+    )
+  end
+
+  def retrive_token(
+        %{client_id: client_id, client_secret: client_secret, base_url: base_url},
+        code,
+        redirect_uri
+      ) do
+    Req.post!(base_url,
+      form: [
+        client_id: client_id,
+        client_secret: client_secret,
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code"
+      ]
+    )
+  end
+
+  def oauth2_authorize_url(
+        %{client_id: client_id, base_url: base_url},
+        scope,
+        redirect_uri
+      )
+      when is_bitstring(scope) do
+    "#{base_url}/application/o/authorize/?client_id=#{client_id}&scope=#{scope}&redirect_uri=#{redirect_uri}"
+  end
+
+  def oauth2_authorize_url(
+        client,
+        scope,
+        redirect_uri
+      )
+      when is_list(scope),
+      do:
+        oauth2_authorize_url(
+          client,
+          Enum.join(scope, " "),
+          redirect_uri
+        )
 end
--- mix.exs
+++ mix.exs
@@ -8,7 +8,11 @@ defmodule AuthentikClient.MixProject do
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      name: :authentik_client
+      name: "authentik_client",
+      source_url: "https://github.com/chenyunda218/authentik_client",
+      description: description(),
+      package: package(),
+      deps: deps()
     ]
   end
 
@@ -23,6 +27,22 @@ defmodule AuthentikClient.MixProject do
   defp deps do
     [
       {:req, "~> 0.5.0"}
+    ]
+  end
+
+  defp description() do
+    "AuthentikClient is a client for the Authentik API."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "authentik_client",
+      # These are the default files included in the package
+      # files: ~w(lib priv .formatter.exs mix.exs README* readme* LICENSE*
+      #           license* CHANGELOG* changelog* src),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/chenyunda218/authentik_client"}
     ]
   end
 end
